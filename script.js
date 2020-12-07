1e0ae6455a1a891806bc94e9b27a1db31e9d8bfa
evans oginga <evansoginga7@gmail.com>
2020-12-08 00:03:38 +0300

    updated the logic for the game using js module and functions

--- script.js
+++ script.js
@@ -11,3 +11,79 @@ const winningCombos = [
     [1,4,7],
     [2,5,8]
 ];
+
+
+
+const grid = () => Array.from(document.getElementsByClassName('q'));
+const qNumId = (qEl) => Number.parseInt(qEl.id.replace('q', ''));
+const emptyQs = () => grid().filter(_qEl => _qEl.innerText === '');
+
+/*
+    created a module for playing 
+*/
+
+
+const play = (()=>{
+    const allSame = (arr) => arr.every(_qEl => _qEl.innerText === arr[0].innerText && _qEl.innerText !== '');
+    const takeTurn = (index, letter) => grid()[index].innerText = letter;
+    const opponentChoice = () => qNumId(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
+    const endGame = (winningSequence) => {
+        winningSequence.forEach(_qEl => _qEl.classList.add('winner'));
+        listeners.disableListeners();
+    };
+    const checkForVictory = () => {
+        let victory = false;
+        const _grid = grid();
+        winningCombos.forEach(_c => {
+            const sequence = [_grid[_c[0]], _grid[_c[1]], _grid[_c[2]]];
+            if(allSame(sequence)) {
+                victory = true;
+                endGame(sequence);
+            }
+        });
+        return victory;
+    };
+
+    const opponentTurn = () => {
+        listeners.disableListeners();
+        setTimeout(() => {
+            takeTurn(opponentChoice(), 'o');
+            if(!checkForVictory())
+                listeners.enableListeners();
+
+        }, 1000);
+    };
+
+    return {
+        allSame,
+        takeTurn,
+        opponentChoice,
+        endGame,
+        checkForVictory,
+        opponentTurn
+    }
+
+
+})();
+
+
+const clickFn = ($event) => {
+    play.takeTurn(qNumId($event.target), 'x');
+    if(!play.checkForVictory())
+        play.opponentTurn();
+};
+
+//created a module for the listeners
+
+const listeners = (() => {
+    const enableListeners = () => grid().forEach(_qEl => _qEl.addEventListener('click', clickFn));
+    const disableListeners = () => grid().forEach(_qEl => _qEl.removeEventListener('click', clickFn));
+
+    return {
+        enableListeners,
+        disableListeners
+    }
+
+})();
+
+listeners.enableListeners();
