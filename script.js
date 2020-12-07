8a8e87d73b2fba42d0b914f020c1b1a04474ee57
evans oginga <evansoginga7@gmail.com>
2020-12-07 23:32:33 +0300

    added all the possible winning combinations

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+    All the possible combinations for winning the game
+*/
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+];
